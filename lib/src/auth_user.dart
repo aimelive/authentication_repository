e6187f5fb9c49d08502192eaf580fde5d3bc6b4f
aimelive <aimendayambaje24@gmail.com>
2023-01-31 13:48:21 +0200

    Initial commit

---
+++ lib/src/auth_user.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class AuthUser extends Equatable {
+  final String id;
+  final String? email;
+  final String? name;
+  final bool emailVerified;
+
+  const AuthUser({
+    required this.id,
+    this.email,
+    this.name,
+    this.emailVerified = false,
+  });
+
+  static const empty = AuthUser(id: '');
+
+  //Create a getter method to check if user is empty or not
+  bool get isEmpty => this == AuthUser.empty;
+
+  @override
+  List<Object?> get props => [email, id, name, emailVerified];
+}
